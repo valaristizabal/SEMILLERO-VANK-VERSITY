39e37541ebebffa8f68ac948f6f86082c7cada4e
Valeria <119232403+valaristizabal@users.noreply.github.com>
2024-10-28 11:29:38 -0500

    semana 2 algoritmos 3 ejercicios completos

--- EJERCICIOS CLASE 15 DE OCTUBRE/GUÍA VANKVERSITY SEMANA 2 ALGORITMOS 3/CÓDIGO GUÍA VANKVERSITY SEMANA 2 ALGORITMOS 3/EJERCICIO 2 DE SWITCH/script.js
+++ EJERCICIOS CLASE 15 DE OCTUBRE/GUÍA VANKVERSITY SEMANA 2 ALGORITMOS 3/CÓDIGO GUÍA VANKVERSITY SEMANA 2 ALGORITMOS 3/EJERCICIO 2 DE SWITCH/script.js
@@ -6,4 +6,20 @@ su categoría. Tenga en cuenta lo siguiente:
 ● La categoría 2 tendrá un aumento salarial del 25%.
 ● La categoría 3 tendrá un aumento salarial del 45%.*/
 
-let categoria = parseInt(prompt("Ingrese la categoría del empleado"))
+const SALARIO = 1300000;
+let categoria = parseInt(prompt("Ingrese la categoría del empleado"));
+let resultadoSalario;
+switch(categoria){
+    case 1: 
+        resultadoSalario = SALARIO + (SALARIO *0.15);
+        break;
+    case 2: 
+        resultadoSalario = SALARIO + (SALARIO *0.25);
+        break;
+    case 3: 
+        resultadoSalario = SALARIO + (SALARIO *0.45);
+        break;
+}
+
+
+console.log("El aumento de salario es: " + resultadoSalario);
